191f2406da60fd54d4e0b89be50c769a49ed1ee7
Andrew <awrigh206@caledonian.ac.uk>
2019-12-26 22:52:45 +0000

    Added a new type of area, it does not have any additional functionality yet, but it will use a Riddle to let the player through or not depending on how the player does in said Riddle

---
+++ src/main/java/com/andrew/textadventure/Areas/RiddleArea.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.andrew.textadventure.Areas;
+
+import com.andrew.textadventure.Helpers.Choice;
+import com.andrew.textadventure.Player.Player;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Andrew Wright
+ */
+public class RiddleArea extends Area implements IArea 
+{
+
+    public RiddleArea() 
+    {
+        super();
+        this.openingLine = "Welcome to the Riddle area, hope you manage to solve it!";
+    }
+    @Override
+    public boolean canEnter(Player player) {
+        return true;
+    }
+
+    @Override
+    public ArrayList<Choice> getAreaChoices() {
+        return super.getGenericChoices();
+    }
+
+    @Override
+    public String getOpeningLine() {
+        return this.openingLine;
+    }
+    
+}
